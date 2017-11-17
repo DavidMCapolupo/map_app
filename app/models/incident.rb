d1d9d3ca297f812875239f5198356a8d042f53d0
David Capolupo <david.m.capolupo@gmail.com>
2017-11-17 22:05:37 +0000

    added code to incident model

--- app/models/incident.rb
+++ app/models/incident.rb
@@ -1,4 +1,15 @@
 class Incident < ApplicationRecord
-#     geocoded_by :full_street_address
+#     include Google
+#     has_many :users
+#     has_many :places
+#     geocoded_by :location
 #     after_validation :geocode
+#     before_save :setup
+#     
+    #     def setup
+        #     geocode if self.latitude.nil? or self.longitude.nil?
+        #   rescue => e
+            #     puts "#{e.message} Unable to geocode"
+        #     end
+          # end
 end
